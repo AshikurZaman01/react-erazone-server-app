34456f7723be06c99af4e2c9434a8d6b2c925b53
AshikurZaman01 <ashikurzaman774@gmail.com>
2023-10-20 01:54:25 +0600

    add post method

--- index.js
+++ index.js
@@ -35,12 +35,24 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const erazoneCollection = client.db("ErazoneDb").collection("erazone");
+
+    //POST DATA
+    app.post('/erazone', async (req, res) =>{
+        const data = req.body;
+        const result = await erazoneCollection.insertOne(data);
+        res.send(result);
+        console.log(result);
+    })
+    //POST DATA END
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
