ede3bd01d713288ccde7034d956819dc5ee392f8
AshikurZaman01 <ashikurzaman774@gmail.com>
2023-10-20 16:13:36 +0600

    add get data

--- index.js
+++ index.js
@@ -65,6 +65,16 @@ async function run() {
     })
     //DELETE DATA end
 
+    //UPDATE DATA
+    app.get('/erazone/:id', async (req, res) =>{
+        const id = req.params.id;
+        const result = await erazoneCollection.findOne({_id: new ObjectId (id)});
+        res.send(result);
+        console.log(result);
+    })
+    //UPDATE DATA end
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
